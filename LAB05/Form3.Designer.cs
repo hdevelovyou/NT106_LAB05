460ed5a502944c265fbe5f3c606726ce430cf4bf
faviconico1910 <23521179@gm.uit.edu.vn>
2025-06-02 23:06:58 +0700

    add bai3

---
+++ LAB05/Form3.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace LAB05
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tbTo = new TextBox();
+            tbFrom = new TextBox();
+            tbSubject = new TextBox();
+            tbPasswd = new TextBox();
+            tbBody = new RichTextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            btnSend = new Button();
+            SuspendLayout();
+            // 
+            // tbTo
+            // 
+            tbTo.Location = new Point(149, 107);
+            tbTo.Name = "tbTo";
+            tbTo.Size = new Size(234, 27);
+            tbTo.TabIndex = 0;
+            // 
+            // tbFrom
+            // 
+            tbFrom.Location = new Point(149, 34);
+            tbFrom.Name = "tbFrom";
+            tbFrom.Size = new Size(234, 27);
+            tbFrom.TabIndex = 0;
+            // 
+            // tbSubject
+            // 
+            tbSubject.Location = new Point(149, 216);
+            tbSubject.Name = "tbSubject";
+            tbSubject.Size = new Size(846, 27);
+            tbSubject.TabIndex = 1;
+            // 
+            // tbPasswd
+            // 
+            tbPasswd.Location = new Point(740, 34);
+            tbPasswd.Name = "tbPasswd";
+            tbPasswd.Size = new Size(255, 27);
+            tbPasswd.TabIndex = 2;
+            // 
+            // tbBody
+            // 
+            tbBody.Location = new Point(149, 314);
+            tbBody.Name = "tbBody";
+            tbBody.Size = new Size(846, 265);
+            tbBody.TabIndex = 3;
+            tbBody.Text = "";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(37, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 20);
+            label1.TabIndex = 4;
+            label1.Text = "From";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(37, 114);
+            label2.Name = "label2";
+            label2.Size = new Size(25, 20);
+            label2.TabIndex = 5;
+            label2.Text = "To";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(634, 41);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Password";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(37, 219);
+            label4.Name = "label4";
+            label4.Size = new Size(58, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Subject";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(37, 314);
+            label5.Name = "label5";
+            label5.Size = new Size(43, 20);
+            label5.TabIndex = 8;
+            label5.Text = "Body";
+            // 
+            // btnSend
+            // 
+            btnSend.Location = new Point(1032, 407);
+            btnSend.Name = "btnSend";
+            btnSend.Size = new Size(94, 62);
+            btnSend.TabIndex = 9;
+            btnSend.Text = "Send";
+            btnSend.UseVisualStyleBackColor = true;
+            btnSend.Click += btnSend_Click;
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1157, 624);
+            Controls.Add(btnSend);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(tbBody);
+            Controls.Add(tbPasswd);
+            Controls.Add(tbSubject);
+            Controls.Add(tbFrom);
+            Controls.Add(tbTo);
+            Name = "Form3";
+            Text = "Form3";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox tbTo;
+        private TextBox tbFrom;
+        private TextBox tbSubject;
+        private TextBox tbPasswd;
+        private RichTextBox tbBody;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button btnSend;
+    }
+}
---
+++ LAB05/Form3.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MimeKit;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+
+namespace LAB05
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnSend_Click(object sender, EventArgs e)
+        {
+            string fromEmail = tbFrom.Text;
+            string toEmail = tbTo.Text;
+            string subject = tbSubject.Text;
+            string body = tbBody.Text;
+            string password = tbPasswd.Text;
+            try
+            {
+                var message = new MimeMessage();
+                message.From.Add(MailboxAddress.Parse(fromEmail));
+                message.To.Add(MailboxAddress.Parse(toEmail));
+                message.Subject = subject;
+                message.Body = new TextPart("plain") { Text = body };
+
+                using var smtp = new SmtpClient();
+                await smtp.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+                await smtp.AuthenticateAsync(fromEmail, password);
+                await smtp.SendAsync(message);
+                await smtp.DisconnectAsync(true);
+
+                MessageBox.Show("Email đã gửi thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Gửi email thất bại:\n{ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
